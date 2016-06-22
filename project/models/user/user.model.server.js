aed36e7c6febb88cd3d184124b418df24b109c4b
cmiram <nyyankeeschris@gmail.com>
2016-06-22 16:44:57 -0400

    implemented user model

--- project/models/user/user.model.server.js
+++ project/models/user/user.model.server.js
@@ -23,5 +23,62 @@ module.exports = function() {
         return User.create(user);
     }
 
-    
-}
+    function findUserById(userId) {
+        return User.findOne({_id: userId});
+    }
+
+    function findUserByUsername(username) {
+        return User.findOne({username: username});
+    }
+
+    function findUserByCredentials(username, password) {
+        return User.findOne({username: username, password: password});
+    }
+
+    function updateUser(userId, newUser) {
+        return User.update(
+            {_id: userId},
+            { $set: newUser}
+        );
+    }
+
+    function deleteUser(userId) {
+        return User.remove({_id: userId});
+    }
+
+    function pushShow(userId, showId) {
+        return User.update(
+            {_id: userId},
+            { $pushAll: {shows: [showId]}}
+        );
+    }
+
+    function pullShow(userId, showId) {
+        return User.update(
+            {_id: userId},
+            {$pullAll: {shows: [showId]}}
+        );
+    }
+
+    function pushFollow(userId, followId) {
+        return User.update(
+            {_id: userId},
+            {$pushAll: {follows: [followId]}}
+        );
+    }
+
+    function pullFollow(userId, followId) {
+        return User.update(
+            {_id: userId},
+            {$pullAll: {follows: [followId]}}
+        );
+    }
+
+    function findUserByFacebookId(facebookId) {
+        return User.findOne({'facebook.id': facebookId});
+    }
+
+    function findUserByGoogleId(googleId) {
+        return User.findOne({'google.id': googleId});
+    }
+};

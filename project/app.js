471b991bcdda048523993cea8c6c070f4f071d7d
cmiram <nyyankeeschris@gmail.com>
2016-06-22 16:55:25 -0400

    added npm dependencies

--- project/app.js
+++ project/app.js
@@ -1,12 +1,8 @@
 module.exports = function(app) {
-<<<<<<< HEAD
-    var models = require("./models/models.server.js")();
 
-    require("./services/user.service.server.js")(app, models);
-};
-=======
     var models = require("./models/models.server.js");
     
-    
-}
->>>>>>> added directory structure for serverside files
+
+    var userService = require('./services/user.service.server')(app, models);
+};
+

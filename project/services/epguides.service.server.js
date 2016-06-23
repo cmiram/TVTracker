999d79f50f8ae0f626834aece9b9cb3a3779986d
cmiram <nyyankeeschris@gmail.com>
2016-06-23 14:46:43 -0400

    added some error handling to serverside epguides api service

--- project/services/epguides.service.server.js
+++ project/services/epguides.service.server.js
@@ -14,14 +14,24 @@ module.exports = function(app) {
     function showMetadata(req, res) {
         var show = req.params.show;
         request(baseUrl + '/show/' + show + '/info/', function(error, response, body) {
-            res.json(body);
+            if(!error && response.statusCode == 200) {
+                res.json(body);
+            }
+            else {
+                res.status(400).send('error with api call');
+            }
         });
     }
 
     function allEpisodes(req, res) {
         var show = req.params.show;
         request(baseUrl + '/show/' + show + '/', function(error, response, body) {
-            res.json(body);
+            if(!error && response.statusCode == 200) {
+                res.json(body);
+            }
+            else {
+                res.status(400).send('error with api call');
+            }
         });
     }
 
@@ -30,21 +40,36 @@ module.exports = function(app) {
         var season = req.params.season;
         var episode = req.params.episode;
         request(baseUrl + '/show/' + show + '/' + season + '/' + episode + '/', function(error, response, body) {
-            res.json(body);
+            if(!error && response.statusCode == 200) {
+                res.json(body);
+            }
+            else {
+                res.status(400).send('error with api call');
+            }
         });
     }
 
     function nextEpisode(req, res) {
         var show = req.params.show;
         request(baseUrl + '/show/' + show + '/next/', function(error, response, body) {
-            res.json(body);
+            if(!error && response.statusCode == 200) {
+                res.json(body);
+            }
+            else {
+                res.status(400).send('error with api call');
+            }
         });
     }
 
     function lastEpisode(req, res) {
         var show = req.params.show;
         request(baseUrl + '/show/' + show + '/last/', function(error, response, body) {
-            res.json(body);
+            if(!error && response.statusCode == 200) {
+                res.json(body);
+            }
+            else {
+                res.status(400).send('error with api call');
+            }
         });
     }
 };

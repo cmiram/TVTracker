5d117d1bb5f0dc9411e22c0de3bcc3ac1493b683
cmiram <nyyankeeschris@gmail.com>
2016-06-27 00:34:42 -0400

    removed ejs folders

--- browseShows/app.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-    app.get('/browseShows/browse', browseHandler);
-    //var tmdbService = require('/project/services/tmdb.service.server')(app);
-
-    function browseHandler(req, res) {
-        res.render('browseShows/browse')
-    }
-};

521acc3892abc261556f155c0283e92644ec1244
Chris Miramontes <nyyankeeschris@gmail.com>
2017-04-13 18:12:33 -0400

    removed extra empty line

--- project/app.js
+++ project/app.js
@@ -1,7 +1,6 @@
 module.exports = function(app) {
     var models = require("./models/models.server.js")();
     
-    
     var userService = require('./services/user.service.server')(app, models);
     var epguidesService = require('./services/epguides.service.server')(app);
     var tmdbService = require('./services/tmdb.service.server')(app);

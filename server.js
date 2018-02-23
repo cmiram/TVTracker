274b2eb5332f88ffa6d01c55ba4c4496986563fa
Chris Miramontes <nyyankeeschris@gmail.com>
2018-02-22 19:00:30 -0500

    updated ip/port env variable names for openshift 3

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3005;
+var ipaddress = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var passport = require('passport');
 var session = require('express-session');
 
